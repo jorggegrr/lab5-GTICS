bfbe43375a15b3912fd00f50689c924f0e3a12ba
Miguel <a20190050@pucp.edu.pe>
2022-10-04 21:56:19 -0500

    a

--- src/main/java/com/pucp/lab5gtics/repository/EmployeeRepository.java
+++ src/main/java/com/pucp/lab5gtics/repository/EmployeeRepository.java
@@ -11,9 +11,13 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
-    @Query(value = "SELECT * FROM employees where first_name like %?1%",
+    @Query(value = "SELECT * FROM employees where first_name like %?1% ",
             nativeQuery = true)
     List<Employee> buscarNombre(String nombre);
+
+    @Query(value="select * from employees where email= ?1 ;",nativeQuery = true)
+    Employee employeeCorreo(String codigo);
+
 
     @Query(value = "SELECT * FROM employees order by first_name desc",
             nativeQuery = true)
