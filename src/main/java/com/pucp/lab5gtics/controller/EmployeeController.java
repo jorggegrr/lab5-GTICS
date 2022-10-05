2a2d625fd9bc14cd2d27a132cb98144339b9156b
david34Y <dgonzalesn@pucp.edu.pe>
2022-10-04 21:01:31 -0500

    Comit davic

--- src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
+++ src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
@@ -1,15 +1,24 @@
 package com.pucp.lab5gtics.controller;
 
+import com.pucp.lab5gtics.entity.Employee;
+import com.pucp.lab5gtics.repository.DepartmentRepository;
+import com.pucp.lab5gtics.repository.EmployeeRepository;
+import com.pucp.lab5gtics.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class EmployeeController {
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    DepartmentRepository departmentRepository;
+    @Autowired
+    JobRepository jobRepository;
 
     @GetMapping({"empleado/lista", "empleado"})
     public String listEmployee(Model model, @RequestParam(name = "search",required = false) String search, @RequestParam(name = "order", required = false) Integer order, RedirectAttributes attributes){
@@ -34,15 +43,17 @@ public class EmployeeController {
     }
 
     //Guardar Empleado
-    //@...Mapping("")
+    @PostMapping("empleado/guardar")
     public String saveEmployee(  ) {
         //        COMPLETAR
         return "XXXXXX";
     }
 
     //Nuevo Empleado
-    public String newEmployee(Model model) {
-        //        COMPLETAR
-        return "XXXXXX";
+    @GetMapping("employee/nuevo")
+    public String newEmployee(@ModelAttribute("employee") Employee employee,
+                                Model model) {
+        model.addAttribute();
+        return "employee/datos";
     }
 }
--- src/main/java/com/pucp/lab5gtics/repository/JobRepository.java
+++ src/main/java/com/pucp/lab5gtics/repository/JobRepository.java
@@ -3,5 +3,6 @@ package com.pucp.lab5gtics.repository;
 import org.springframework.stereotype.Repository;
 
 //Completar
+@Repository
 public interface JobRepository {
 }
